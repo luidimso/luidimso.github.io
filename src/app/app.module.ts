ebd8ffd01642d451f928e0029108011b6dd3bd89
luidimso <luidi_mso@outlook.com>
2020-08-07 13:32:48 -0300

    Estruturação da página principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
